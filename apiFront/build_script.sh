fe69dec6e0ca1dc61d4f452b4edbfc41485d0da2
peterm85 <petermichael85@gmail.com>
2021-05-24 10:55:26 +0200

    Update functional release deployment

---
+++ apiFront/build_script.sh
@@ -0,0 +1,3 @@
+docker build -f Dockerfile -t petermichael85/backend:0.0.4 .
+
+docker push petermichael85/backend:0.0.4
---
+++ backend/build_script.sh
@@ -0,0 +1,3 @@
+docker build -f Dockerfile -t petermichael85/apifront:0.0.3 .
+
+docker push petermichael85/apifront:0.0.3
